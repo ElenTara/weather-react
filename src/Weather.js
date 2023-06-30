243a1e2a9fc1d83d7a934cb253fa454a2d05944d
Elen Tara <elentara220@gmail.com>
2023-06-30 23:39:42 +0300

    Added AXIOS integration for weather of the city

---
+++ src/Weather.js
@@ -0,0 +1,25 @@
+import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+  let apiKey = "b39fd544fbfd289a17b0d205d5515953";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
+}
